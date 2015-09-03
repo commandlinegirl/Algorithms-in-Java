38129d3acc74429fc573df7b3ccab4e2dad2bce7
Aleksandra Zalcman <biokere@gmail.com>
2015-09-04 01:59:05 +0200

    Adding silent narrowing type conversions examples in compound expressions

--- puzzlers/Multicast.java
+++ puzzlers/Multicast.java
@@ -1,6 +1,6 @@
 public class Multicast {
 
-/* Test conversions between primitive data types.
+/* Test conversions between primitive data types (based on Java Puzzlers by J. Bloch)
  *
  * Java primitive data types 
  * byte - 8-bit, signed
@@ -11,10 +11,10 @@ public class Multicast {
  * double - single-precision 64-bit IEEE 754 floating point
  * char - 16-bit unicode character, unsigned
  * boolean - true/false; min value of '\u0000' (or 0); maximum value of '\uffff' (or 65,535 inclusive)
+ *
  */
 
     public static void main(String[] args) {
-        // Example from Java Puzzlers
         System.out.println((int) (char) (byte) -1);
 
         // Cast int (-1) to byte - narrowing primitive conversion
@@ -29,5 +29,25 @@ public class Multicast {
         // regardless of the target type
         System.out.println((int) (char) (byte) (-1));
         assert ((int) (char) (byte) (-1)) == Math.pow(2, 16) - 1;
+
+        // Compound assignment expression E1 op= E2
+        // is equivalent to simple: T E1 = (T) (E1 op E2), and
+        // E1 is evaluated only once
+        byte x = 0;
+        int i = 3333333;
+        x += i; // hidden cast!
+        System.out.println("Unexpected x value after silent conversion to byte: " + x);
+        assert x == -43;
+
+        int y = 20;
+        double j = 0.5;
+        y += j; // hidden cast!
+        assert y == 20;
+        System.out.println("Unexpected y value after silent narrowing cast to int: " + y);
+
+        Object n = "The cat ";
+        String m = "is grumpy."; 
+        n += m;
+        System.out.println(n);
     }
 }
