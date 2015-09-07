577507864240afdc21d4d0325bb2b30471c98d8a
Aleksandra Zalcman <biokere@gmail.com>
2015-09-07 12:52:28 +0200

    Adding a Dijkstra algorithm implementation

--- src/com/commandlinegirl/algorithms/datastructures/Graph.java
+++ src/com/commandlinegirl/algorithms/datastructures/Graph.java
@@ -16,22 +16,29 @@ public class Graph {
         return root;
     }
     
-    public static class Vertex {
+    public static class Vertex implements Comparable<Vertex> {
 
+        private final int id;
         private Set<Vertex> adjacencies = new HashSet<>();
-        private int id;
+        private Set<Edge> edges = new HashSet<>();
         private boolean visited = false;
+        private int minDistance = Integer.MAX_VALUE;
+        private Vertex previous;
         
         public Vertex(int id) {
             this.id = id;
+        }
+        
+        public Set<Vertex> getAdjacencies() {
+            return Collections.unmodifiableSet(adjacencies);
         }
         
         public void addAdjacent(Vertex vertex) {
             adjacencies.add(vertex);
         }
         
-        public Set<Vertex> getAdjacencies() {
-            return Collections.unmodifiableSet(adjacencies);
+        public Set<Edge> getEdges() {
+            return Collections.unmodifiableSet(edges);
         }
         
         public void setVisited() {
@@ -45,5 +52,57 @@ public class Graph {
         public int getId() {
             return id;
         }
+        
+        public Vertex getPrevious() {
+            return previous;
+        }
+
+        @Override
+        public int compareTo(Vertex other) {
+            return Integer.compare(minDistance , other.minDistance);
+        }
+
+        public void setMinDistance(int i) {
+            minDistance = 0;
+        }
+        
+        public int getMinDistance() {
+            return minDistance;
+        }
+
+        public void setPrevious(Vertex vertex) {
+            previous = vertex;
+            
+        }
+    }
+    
+    public static class Edge implements Comparable<Edge> {
+        private final Vertex source;
+        private final Vertex destination;
+        private final int weight;
+        
+        public Edge(Vertex source, Vertex destination, int weight) {
+            this.source = source;
+            this.destination = destination;
+            this.weight = weight;
+        }
+        
+        @Override
+        public int compareTo (Edge other) {
+            return Integer.compare(weight, other.weight);
+        }
+
+        public Vertex getSource() {
+            return source;
+        }
+
+        public Vertex getDestination() {
+            return destination;
+        }
+
+        public int getWeight() {
+            return weight;
+        }
+
     }
 }
---
+++ src/com/commandlinegirl/algorithms/graphs/Dijkstra.java
@@ -0,0 +1,45 @@
+package com.commandlinegirl.algorithms.graphs;
+
+import java.util.Map;
+import java.util.PriorityQueue;
+
+import com.commandlinegirl.algorithms.datastructures.Graph.Edge;
+import com.commandlinegirl.algorithms.datastructures.Graph.Vertex;
+
+/* Dijkstra algorithm implementation for finding the minimum distance
+ * for a reference vertex to all other vertices. */
+public class Dijkstra {
+
+	/* Main algorithm */ 
+	public void search(Vertex start) {
+		start.setMinDistance(0);
+		PriorityQueue<Vertex> queue = new PriorityQueue<Vertex>();
+		queue.add(start);
+		while (!queue.isEmpty()) {
+			Vertex u = queue.poll();
+			for (Edge e : u.getEdges()) {
+				Vertex v = e.getDestination();
+				int weight = e.getWeight();
+				int distanceViaU = u.getMinDistance() + weight;
+				if (distanceViaU < v.getMinDistance()) {
+					queue.remove(v); 
+					v.setMinDistance(distanceViaU);
+					v.setPrevious(u);
+					queue.add(v); 
+				} 
+			}
+		}
+	}
+
+	public void printDistances(Map<Integer, Vertex> nodes, int start) {
+		StringBuilder sb = new StringBuilder();
+		for (Map.Entry<Integer, Vertex> entry : nodes.entrySet()) {
+			if (entry.getKey() != start) {
+				Vertex node = entry.getValue();
+				Integer dist = node.getMinDistance() == Integer.MAX_VALUE ? -1 : node.getMinDistance();
+				sb.append(dist + " "); 
+			}
+		}
+		System.out.println(sb.toString().trim());
+	}	
+}
