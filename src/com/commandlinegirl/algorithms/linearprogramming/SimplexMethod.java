1286f242f3517a09c85ce0d8d0ad32884cc74582
Aleksandra Zalcman <biokere@gmail.com>
2016-05-05 18:19:40 +0200

    Added pivot searching method to simplex algorithm

--- src/com/commandlinegirl/algorithms/linearprogramming/SimplexMethod.java
+++ src/com/commandlinegirl/algorithms/linearprogramming/SimplexMethod.java
@@ -1,44 +1,64 @@
 package com.commandlinegirl.algorithms.linearprogramming;
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.List;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 public class SimplexMethod {
 
-    private static Character DELIMINATOR = ',';
+    private static final Logger logger = Logger.getLogger(SimplexMethod.class.getName());
 
-    private List<List<Integer>> readInput(String fileName, Character deliminator) {
-        File file = new File(fileName);
-        List<List<Integer>> input = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                List<Integer> equation = convertToInt(Arrays.asList(line.split(deliminator.toString())));
-                input.add(equation);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+    private int getMinIndex(List<Integer> list) {
+        int minIndex = 0;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) < list.get(minIndex))
+                minIndex = i;
         }
-        return input;
+        return minIndex;
     }
 
-    private List<Integer> convertToInt(List<String> strings) {
-        return strings.stream().map(Integer::parseInt).collect(Collectors.toList());
+
+    private int[][] getPivot(List<List<Integer>> tableau) {
+        int pivotColumn =  getMinIndex(tableau.get(0));
+        int pivotRow = getPivotRow(tableau, pivotColumn);
+        return new int[][]{{pivotRow}, {pivotColumn}};
+    }
+
+    private int getPivotRow(List<List<Integer>> tableau, int pivotColumnIndex) {
+        int minValue = 0;
+        int minIndex = 0;
+        List<Integer> valueColumn = getColumn(tableau, tableau.size() - 1);
+        List<Integer> pivotColumn = getColumn(tableau, pivotColumnIndex);
+        for (int i = 0; i < valueColumn.size(); i++) {
+            if (pivotColumn.get(i) != 0 && valueColumn.get(i) / pivotColumn.get(i) < minValue) {
+                minValue = valueColumn.get(i) / pivotColumn.get(i);
+                minIndex = i;
+            }
+        }
+        return minIndex;
+    }
+
+    private List<Integer> getColumn(List<List<Integer>> tableau, int columnIndex) {
+        if (Preconditions.checkIndexNotWithinArray(tableau, columnIndex))
+            throw new IllegalArgumentException("Invalid column index " + columnIndex + " for tableau " + tableau);
+        return tableau.stream().map(e -> e.get(columnIndex)).collect(Collectors.toList());
+    }
+
+    private void maximize(List<List<Integer>> tableau) {
+        int[][] pivot = getPivot(tableau);
     }
 
     public static void main(String... args) {
+        if (args.length == 0) {
+            logger.severe("Usage: java SimplexMethod <name-of_file>");
+            System.exit(0);
+        }
 
         SimplexMethod simplex = new SimplexMethod();
-
-        System.out.println(args.length);
-        if (args.length > 0) {
-            for (List<Integer> li : simplex.readInput(args[0], DELIMINATOR)) {
-                System.out.println(Arrays.toString(li.toArray()));
-            }
-        }
+        InputReader freader = new InputReader();
+        List<List<Integer>> tableau = freader.readInput(args[0]);
+        simplex.maximize(tableau);
 
     }
 
