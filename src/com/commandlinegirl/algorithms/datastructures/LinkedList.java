be60791bc69a2e8b643f3cc5dff6554442b84cd4
Aleksandra Zalcman <biokere@gmail.com>
2016-09-30 11:55:48 -0700

    Reversing linked list recursively

--- src/com/commandlinegirl/algorithms/datastructures/LinkedList.java
+++ src/com/commandlinegirl/algorithms/datastructures/LinkedList.java
@@ -2,7 +2,7 @@ package com.commandlinegirl.algorithms.datastructures;
 
 public class LinkedList<T> {
 
-    public static Node reverse(Node start) {
+    public static Node reverseIterative(Node start) {
         if (start == null)
             return null;
 
@@ -17,6 +17,30 @@ public class LinkedList<T> {
         }
 
         return prev;
+    }
+
+    public static Node reverseRecursive1(Node start) {
+        return reverseRecursiveHelper(null, start);
+    }
+
+    private static Node reverseRecursiveHelper(Node parent, Node child) {
+        if (child == null)
+            return parent;
+
+        Node next = child.next;
+        child.next = parent;
+        return reverseRecursiveHelper(child, next);
+    }
+
+
+    public static Node reverseRecursive2(Node start) {
+        if (start == null || start.next == null)
+            return start;
+
+        Node previousEnd = reverseRecursive2(start.next);
+        start.next.next = start;
+        start.next = null;
+        return previousEnd;
     }
 
     public static class Node<T> {
--- src/com/commandlinegirl/algorithms/datastructures/test/TestLinkedList.java
+++ src/com/commandlinegirl/algorithms/datastructures/test/TestLinkedList.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class TestLinkedList {
 
     @Test
-    public void testReverseList_full_list() {
+    public void testReverseList_iter_full_list() {
         LinkedList.Node<String> a = new LinkedList.Node<>("a");
         LinkedList.Node<String> b = new LinkedList.Node<>("b");
         LinkedList.Node<String> c = new LinkedList.Node<>("c");
@@ -16,7 +16,7 @@ public class TestLinkedList {
         b.setNext(c);
         c.setNext(d);
 
-        LinkedList.Node newStart = LinkedList.reverse(a);
+        LinkedList.Node newStart = LinkedList.reverseIterative(a);
         Assert.assertEquals("d", newStart.data());
         Assert.assertEquals("c", newStart.getNext().data());
         Assert.assertEquals("b", newStart.getNext().getNext().data());
@@ -24,11 +24,38 @@ public class TestLinkedList {
     }
 
     @Test
-    public void testReverseList_one_element_list() {
+    public void testReverseList_iter_one_element_list() {
         LinkedList.Node<String> a = new LinkedList.Node<>("a");
 
-        LinkedList.Node newStart = LinkedList.reverse(a);
+        LinkedList.Node newStart = LinkedList.reverseIterative(a);
         Assert.assertEquals("a", newStart.data());
         Assert.assertNull(newStart.getNext());
     }
+
+    @Test
+    public void testReverseList_recur_full_list() {
+        LinkedList.Node<String> a = new LinkedList.Node<>("a");
+        LinkedList.Node<String> b = new LinkedList.Node<>("b");
+        LinkedList.Node<String> c = new LinkedList.Node<>("c");
+        LinkedList.Node<String> d = new LinkedList.Node<>("d");
+        a.setNext(b);
+        b.setNext(c);
+        c.setNext(d);
+
+        LinkedList.Node newStart = LinkedList.reverseRecursive1(a);
+        Assert.assertEquals("d", newStart.data());
+        Assert.assertEquals("c", newStart.getNext().data());
+        Assert.assertEquals("b", newStart.getNext().getNext().data());
+        Assert.assertEquals("a", newStart.getNext().getNext().getNext().data());
+    }
+
+    @Test
+    public void testReverseList_recur_one_element_list() {
+        LinkedList.Node<String> a = new LinkedList.Node<>("a");
+
+        LinkedList.Node newStart = LinkedList.reverseRecursive1(a);
+        Assert.assertEquals("a", newStart.data());
+        Assert.assertNull(newStart.getNext());
+    }
+
 }
