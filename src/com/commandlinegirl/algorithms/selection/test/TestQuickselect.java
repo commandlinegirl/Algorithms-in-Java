ae3c3becd421d1e2faf5a4bf9a7b4e2d6169e985
Aleksandra Zalcman <biokere@gmail.com>
2016-10-05 20:06:50 -0700

    Expected result update

--- src/com/commandlinegirl/algorithms/selection/test/TestQuickselect.java
+++ src/com/commandlinegirl/algorithms/selection/test/TestQuickselect.java
@@ -13,7 +13,7 @@ public class TestQuickselect {
         Quickselect qselect = new Quickselect();
         int[] ar = new int[]{10, 4, 8, 3, 2, 5, 10, 1001, 4, 5, 6};
         int result = qselect.getKthSmallestElement(ar, 3);
-        int expected = 5;
+        int expected = 4;
         //int[] expectedSortedArr = new int[]{2, 3, 4, 4, 5, 5, 6, 8, 10, 10, 1001};
         assertEquals(expected, result);
     }
