1114252cbf1de53302d15207465a2fd8d1625405
Aleksandra Zalcman <biokere@gmail.com>
2016-10-17 21:07:47 -0700

    Add a few more linked-list related functions

--- src/com/commandlinegirl/algorithms/datastructures/LinkedList.java
+++ src/com/commandlinegirl/algorithms/datastructures/LinkedList.java
@@ -2,6 +2,11 @@ package com.commandlinegirl.algorithms.datastructures;
 
 public class LinkedList<T> {
 
+    /***
+     * Returns a head of the reversed linked list (iterative implementation).
+     * @param start
+     * @return
+     */
     public static Node reverseIterative(Node start) {
         if (start == null)
             return null;
@@ -19,6 +24,11 @@ public class LinkedList<T> {
         return prev;
     }
 
+    /***
+     * Returns a head of the reversed linked list (recursive implementation 1).
+     * @param start
+     * @return
+     */
     public static Node reverseRecursive1(Node start) {
         return reverseRecursiveHelper(null, start);
     }
@@ -32,7 +42,11 @@ public class LinkedList<T> {
         return reverseRecursiveHelper(child, next);
     }
 
-
+    /***
+     * Returns a head of the reversed linked list (recursive implementation 2).
+     * @param start
+     * @return
+     */
     public static Node reverseRecursive2(Node start) {
         if (start == null || start.next == null)
             return start;
@@ -41,6 +55,38 @@ public class LinkedList<T> {
         start.next.next = start;
         start.next = null;
         return previousEnd;
+    }
+
+    /***
+     * Returns the middle node of a singly-linked list.
+     * @param start
+     * @return
+     */
+    public static Node middleNode(Node start) {
+        if (start == null)
+            return null;
+
+        Node pointer = start;
+        Node runner = start.next;
+
+        while (runner != null && runner.next != null) {
+            pointer = pointer.next;
+            runner = runner.next.next;
+        }
+
+        return pointer;
+    }
+
+    /***
+     * Determine whether a linked list contains a loop as quickly as possible without using any extra storage.
+     * Also, identify the location of the loop.
+     *
+     * Source: http://www.algorist.com/algowiki/index.php/Data-structures-TADM2E
+     * @param start
+     * @return
+     */
+    public static Node loopNode(Node start) {
+        return null;
     }
 
     public static class Node<T> {
