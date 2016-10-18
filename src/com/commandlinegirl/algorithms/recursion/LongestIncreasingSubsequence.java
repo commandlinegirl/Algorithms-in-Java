5dbc9f94b5a2c42712f26b8d8391207d9497e807
Aleksandra Zalcman <biokere@gmail.com>
2016-10-18 14:53:47 -0700

    Longest increasing subsequence

---
+++ src/com/commandlinegirl/algorithms/recursion/LongestIncreasingSubsequence.java
@@ -0,0 +1,28 @@
+package com.commandlinegirl.algorithms.recursion;
+
+/**
+ * Solution to longest increasing subsequence problem
+ * using dynamic programming with memoization and tabulation.
+ */
+public class LongestIncreasingSubsequence {
+
+    public int lisTab(int[] ar) {
+        int n = ar.length;
+        int max = 0;
+        int[] cache = new int[n];
+
+        // for each item i, iterate over elements at indices 0 to i - 1
+        // and cache the lis value at i
+        for (int i = 0; i < n; i++) {
+            cache[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (ar[j] < ar[i])
+                    cache[i] = Math.max(cache[i], 1 + cache[j]);
+            }
+            max = Math.max(max, cache[i]);
+        }
+
+        return max;
+    }
+
+}
---
+++ src/com/commandlinegirl/algorithms/recursion/tests/LongestIncreasingSubsequenceTest.java
@@ -0,0 +1,52 @@
+package com.commandlinegirl.algorithms.recursion.tests;
+
+import com.commandlinegirl.algorithms.recursion.LongestIncreasingSubsequence;
+import com.commandlinegirl.algorithms.recursion.LongestPalindromicSubsequence;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LongestIncreasingSubsequenceTest {
+
+    @Test
+    public void testLongestIncreasingSubsequence_empty() {
+        LongestIncreasingSubsequence lis = new LongestIncreasingSubsequence();
+        int[] ar = {};
+        int res = lis.lisTab(ar);
+        Assert.assertEquals(0, res);
+    }
+
+    @Test
+    public void testLongestIncreasingSubsequence_one_item() {
+        LongestIncreasingSubsequence lis = new LongestIncreasingSubsequence();
+        int[] ar = {8};
+        int res = lis.lisTab(ar);
+        Assert.assertEquals(1, res);
+    }
+
+    @Test
+    public void testLongestIncreasingSubsequence1() {
+        LongestIncreasingSubsequence lis = new LongestIncreasingSubsequence();
+        int[] ar = {10, 22, 9, 33, 21, 50, 41, 60, 80};
+        int res = lis.lisTab(ar);
+        Assert.assertEquals(6, res);
+
+    }
+
+    @Test
+    public void testLongestIncreasingSubsequence2() {
+        LongestIncreasingSubsequence lis = new LongestIncreasingSubsequence();
+        int[] ar =  { 10, 9, 8, 7, 9, 5, 11, 0 };
+        int res = lis.lisTab(ar);
+        Assert.assertEquals(3, res);
+    }
+
+    @Test
+    public void testLongestIncreasingSubsequence3() {
+        LongestIncreasingSubsequence lis = new LongestIncreasingSubsequence();
+        int[] ar = {8, 7, 6, 5, 4, 3};
+        int res = lis.lisTab(ar);
+        Assert.assertEquals(1, res);
+    }
+
+
+}
