c13ad9d419d7aa7dd95aa91f23b0ce749fdb1d13
Aleksandra Zalcman <biokere@gmail.com>
2016-10-17 21:08:20 -0700

    A linked list test

--- src/com/commandlinegirl/algorithms/datastructures/test/TestLinkedList.java
+++ src/com/commandlinegirl/algorithms/datastructures/test/TestLinkedList.java
@@ -58,4 +58,22 @@ public class TestLinkedList {
         Assert.assertNull(newStart.getNext());
     }
 
+    @Test
+    public void testMiddleNode_ok() {
+        LinkedList.Node<String> a = new LinkedList.Node<>("a");
+        LinkedList.Node<String> b = new LinkedList.Node<>("b");
+        LinkedList.Node<String> c = new LinkedList.Node<>("c");
+        LinkedList.Node<String> d = new LinkedList.Node<>("d");
+        LinkedList.Node<String> e = new LinkedList.Node<>("e");
+        LinkedList.Node<String> f = new LinkedList.Node<>("f");
+        a.setNext(b);
+        b.setNext(c);
+        c.setNext(d);
+        d.setNext(e);
+        e.setNext(f);
+
+        LinkedList.Node mid = LinkedList.middleNode(a);
+        Assert.assertEquals("c", mid.data());
+    }
+
 }
