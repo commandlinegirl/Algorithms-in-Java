5149f670e6ff0f1fa7a38194bc4a7537257ebe97
Aleksandra Zalcman <biokere@gmail.com>
2016-10-19 14:49:41 -0700

    Change method name

--- src/com/commandlinegirl/algorithms/dp/tests/TestEditDistance.java
+++ src/com/commandlinegirl/algorithms/dp/tests/TestEditDistance.java
@@ -7,42 +7,42 @@ import org.junit.Test;
 public class TestEditDistance {
 
     @Test
-    public void testKnapsackTab_ok1() {
+    public void testEditDistanceTab_ok1() {
         EditDistance e = new EditDistance();
         int res = e.minEditDistanceTab("abcdef", "azced");
         Assert.assertEquals(3, res);
     }
 
     @Test
-    public void testKnapsackRec_ok1() {
+    public void testEditDistanceRec_ok1() {
         EditDistance e = new EditDistance();
         int res = e.minEditDistanceRec("abcdef", "azced");
         Assert.assertEquals(3, res);
     }
 
     @Test
-    public void testKnapsackTab_ok2() {
+    public void testEditDistanceTab_ok2() {
         EditDistance e = new EditDistance();
         int res = e.minEditDistanceTab("crow", "candy");
         Assert.assertEquals(4, res);
     }
 
     @Test
-    public void testKnapsackRec_ok2() {
+    public void testEditDistanceRec_ok2() {
         EditDistance e = new EditDistance();
         int res = e.minEditDistanceRec("crow", "candy");
         Assert.assertEquals(4, res);
     }
 
     @Test
-    public void testKnapsackTab_empty() {
+    public void testEditDistanceTab_empty() {
         EditDistance e = new EditDistance();
         int res = e.minEditDistanceTab("", "candy");
         Assert.assertEquals(5, res);
     }
 
     @Test
-    public void testKnapsackRec_empty() {
+    public void testEditDistanceRec_empty() {
         EditDistance e = new EditDistance();
         int res = e.minEditDistanceRec("", "candy");
         Assert.assertEquals(5, res);
