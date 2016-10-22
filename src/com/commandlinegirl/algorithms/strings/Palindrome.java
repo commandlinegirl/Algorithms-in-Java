2adf370c97b9d62894eee74bf26e35007f05bc58
Aleksandra Zalcman <biokere@gmail.com>
2016-10-22 08:59:58 -0700

    Updates to javadoc

--- src/com/commandlinegirl/algorithms/strings/Palindrome.java
+++ src/com/commandlinegirl/algorithms/strings/Palindrome.java
@@ -3,16 +3,21 @@ package com.commandlinegirl.algorithms.strings;
 public class Palindrome {
 
     /**
-     * Returns true is the string is a palindrome, otherwise returns false.
+     * Returns true is the substring s[i:j] is a palindrome, otherwise returns false.
      * @param s string
-     * @param i first character
-     * @param j last character
+     * @param i first index in the string
+     * @param j last index in the string
      * @return
      */
     public static boolean isPalindrome(String s, int i, int j) {
         return (i >= j) || s.charAt(i) == s.charAt(j) && isPalindrome(s, i + 1, j - 1);
     }
 
+    /**
+     * Returns true is the string is a palindrome, otherwise returns false.
+     * @param s string
+     * @return
+     */
     public static boolean isPalindrome(String s) {
         return isPalindrome(s, 0, s.length() - 1);
     }
