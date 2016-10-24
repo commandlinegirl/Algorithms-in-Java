cb79ba32282582ac3424428619e9695e1dfd7fd3
Aleksandra Zalcman <biokere@gmail.com>
2016-10-24 16:32:27 -0700

    Insert sort with swaps

--- src/com/commandlinegirl/algorithms/sorting/Insertsort.java
+++ src/com/commandlinegirl/algorithms/sorting/Insertsort.java
@@ -5,7 +5,7 @@ package com.commandlinegirl.algorithms.sorting;
  */
 public class Insertsort implements Sort {
 
-    private void insertSort(int[] ar) {
+    private void insertSort1(int[] ar) {
         if (ar == null) {
             return;
         }
@@ -21,8 +21,30 @@ public class Insertsort implements Sort {
         }
     }
 
+    private void insertSort2(int[] ar) {
+        if (ar == null) {
+            return;
+        }
+
+        for (int i = 1; i < ar.length; i++) {
+            int j = i;
+            while (j > 0 && ar[j - 1] > ar[j]) {
+                swap(ar, i, j);
+                j--;
+            }
+        }
+
+    }
+
+    private void swap(int[] ar, int i, int j) {
+        int tmp = ar[i];
+        ar[i] = ar[j];
+        ar[j] = tmp;
+
+    }
+
     @Override
     public void sort(int[] ar) {
-        insertSort(ar);
+        insertSort1(ar);
     }
 }
