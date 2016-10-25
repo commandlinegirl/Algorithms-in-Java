1ab7cc54c593ad346007a6a2234cdb10edf844d2
Aleksandra Zalcman <biokere@gmail.com>
2016-10-24 20:24:03 -0700

    Add array printer util

---
+++ src/com/commandlinegirl/algorithms/utils/Printer.java
@@ -0,0 +1,23 @@
+package com.commandlinegirl.algorithms.utils;
+
+public class Printer {
+
+
+    public static void printMatrix(int[][] p) {
+        if (p == null)
+            throw new IllegalArgumentException("empty matrix");
+
+        for (int i=0;i<p.length;i++) {
+            for(int j=0;j<p.length;j++)
+                System.out.print("  " + p[i][j]);
+            System.out.println("");
+        }
+    }
+
+    public static void printArray(int[] ar) {
+        for (int i : ar) {
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+    }
+}
