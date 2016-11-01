53d650958b4f7b66383b84218de5607264e07f56
Aleksandra Zalcman <biokere@gmail.com>
2016-11-01 08:38:01 -0700

    Red black tree init

--- src/com/commandlinegirl/algorithms/datastructures/RedBlackTree.java
+++ src/com/commandlinegirl/algorithms/datastructures/RedBlackTree.java
@@ -3,9 +3,10 @@ package com.commandlinegirl.algorithms.datastructures;
 public class RedBlackTree {
 
     public static class Node {
+        public Node parent;
         public Node left;
         public Node right;
-        public Color color; // 0 for black, 1 for red
+        public Color color;
         public int val;
 
         public Node(Color color, int val) {
@@ -33,17 +34,100 @@ public class RedBlackTree {
     }
 
     public Node insert(Node root, int val) {
-        Node n = insertBst(root, val, Color.RED); // insert initially as red
-        if (root.left == null && root.right == null) { // node is root
-            n.color = Color.BLACK;
+        Node node = insertBst(root, val, Color.RED);
+        if (root.left == null && root.right == null) {
+            node.color = Color.BLACK;
         }
-        // the parent is black - trivial
+        // If parent is BLACK, we're done
+        if (node.parent == null || node.parent.color.equals(Color.BLACK))
+            return node;
 
-        // the parent is red
+        // If parent is RED, we check the uncle.
+        Node uncle = getSibling(node.parent);
+        if (uncle != null && uncle.color.equals(Color.RED)) {
+            // if parent's parent is root, recolor, else recolor and recheck for a need to rotate
+            if (isGrandParentRoot(node)) {
+                //TODO recolor
+            }
+            else {
+                //TODO recolor and possibly rotate
+            }
+        }
+        else {
+            rotate(node);
+        }
+        return node;
+    }
 
+    private boolean isGrandParentRoot(Node node) {
+        return node.parent != null &&
+                node.parent.parent != null &&
+                node.parent.parent.parent == null;
+    }
 
+    private void rotate(Node node) {
+        if (node == null || node.parent == null || node.parent.parent == null)
+            return;
+        // RR
+        if (!isLeftChild(node.parent.parent, node.parent) && !isLeftChild(node.parent, node)) {
+            rotateLeft(node, true);
+        }
+        //RL
+        //TODO
+        //LL
+        if (!isLeftChild(node.parent.parent, node.parent) && !isLeftChild(node.parent, node)) {
+            rotateRight(node, true);
+        }
+        //LR
+        //TODO
+    }
 
-        return n;
+    private void rotateLeft(Node node, boolean changeColor) {
+        if (node == null || node.parent == null || node.parent.parent == null)
+            return;
+        Node grandParent = node.parent.parent;
+        grandParent.right = node;
+        node.left = node.parent;
+        node.parent.right = null;
+        if (changeColor) {
+            flipColor(node);
+            flipColor(node.parent);
+        }
+    }
+
+    private void rotateRight(Node node, boolean changeColor) {
+        if (node == null || node.parent == null || node.parent.parent == null)
+            return;
+        Node grandParent = node.parent.parent;
+        grandParent.left = node;
+        node.right = node.parent;
+        node.parent.left = null;
+        if (changeColor) {
+            flipColor(node);
+            flipColor(node.parent);
+        }
+    }
+
+    private void flipColor(Node node) {
+        if (node == null)
+            return;
+        if (node.color.equals(Color.BLACK))
+            node.color = Color.RED;
+        else
+            node.color = Color.BLACK;
+    }
+
+    private Node getSibling(Node node) {
+        if (node == null || node.parent == null)
+            return null;
+        Node parent = node.parent;
+        if (parent.left == node)
+            return node.right;
+        return parent.left; //TODO: recheck
+    }
+
+    private boolean isLeftChild(Node parent, Node node) {
+        return parent.left == node;
     }
 
     public void delete(Node n) {
@@ -52,17 +136,35 @@ public class RedBlackTree {
     public void search(Node n) {
     }
 
+    /**
+     * Returns true if the tree passes color validation.
+     * @param root
+     * @return
+     */
     public boolean validate(Node root) {
         return (root == null ||
                 root.color.equals(Color.BLACK)
-                        && !existsRedChildParent(root)
+                        && !existsRedChildParentRelation(root)
                         && equalBlackPaths(root));
     }
 
+    /**
+     * Returns true if the tree meets a requirement of each root to leaf
+     * paths having equal number of black nodes.
+     * @param root
+     * @return
+     */
     private boolean equalBlackPaths(Node root) {
         return blackHeight(root) != -1;
     }
 
+    /**
+     * Returns -1 is there exists a node for which left node's black
+     * height is not equal to right node's black height. Otherwise returns
+     * the black height of the tree.
+     * @param node
+     * @return
+     */
     private int blackHeight(Node node) {
         if (node == null)
             return 0;
@@ -75,14 +177,19 @@ public class RedBlackTree {
         if (right == -1)
             return -1;
 
-        if (Math.abs(left - right) > 0)
+        if (left != right)
             return -1;
 
         int countBlack = node.color.equals(Color.BLACK) ? 1 : 0;
         return countBlack + left; // left is equal to right so we can take either
     }
 
-    private boolean existsRedChildParent(Node root) {
+    /**
+     * Returns true if there exists a red-red parent-child relationship in the tree.
+     * @param root
+     * @return
+     */
+    private boolean existsRedChildParentRelation(Node root) {
         if (root == null)
             return false;
         if (root.left == null && root.right == null)
@@ -91,8 +198,6 @@ public class RedBlackTree {
             return root.color.equals(Color.RED) && root.right.color.equals(Color.RED);
         if (root.right == null)
             return root.color.equals(Color.RED) && root.left.color.equals(Color.RED);
-        return existsRedChildParent(root.left) || existsRedChildParent(root.right);
-
+        return existsRedChildParentRelation(root.left) || existsRedChildParentRelation(root.right);
     }
-
 }
--- src/com/commandlinegirl/algorithms/datastructures/test/RedBlackTreeTest.java
+++ src/com/commandlinegirl/algorithms/datastructures/test/RedBlackTreeTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 public class RedBlackTreeTest {
 
     @Test
-    public void testIValidate1() {
+    public void testValidate1() {
         Node n0, n1, n2, n3, n4, n5, n6, n7, n8, n9;
 
         n0 = new Node(Color.BLACK, 0);
@@ -36,7 +36,7 @@ public class RedBlackTreeTest {
     }
 
     @Test
-    public void testIValidate2() {
+    public void testValidate2() {
         Node n0, n1, n2;
 
         n0 = new Node(Color.BLACK, 0);
@@ -51,7 +51,7 @@ public class RedBlackTreeTest {
     }
 
     @Test
-    public void testIValidate3() {
+    public void testValidate3() {
         Node n0;
         n0 = new Node(Color.BLACK, 0);
 
@@ -61,7 +61,7 @@ public class RedBlackTreeTest {
     }
 
     @Test
-    public void testIValidate4() {
+    public void testValidate4() {
         Node n0, n1, n2, n3;
 
         n0 = new Node(Color.BLACK, 0);
@@ -75,5 +75,28 @@ public class RedBlackTreeTest {
         RedBlackTree b = new RedBlackTree();
         boolean valid = b.validate(n0);
         Assert.assertFalse(valid);
+    }
+
+    @Test
+    public void testValidate5() {
+        Node n0, n1, n2, n3, n4, n5, n6, n7, n8, n9;
+
+        n0 = new Node(Color.BLACK, 0);
+        n1 = new Node(Color.BLACK, 1);
+        n2 = new Node(Color.RED, 2);
+        n3 = new Node(Color.RED, 3);
+        n4 = new Node(Color.BLACK, 4);
+        n5 = new Node(Color.RED, 5);
+        n6 = new Node(Color.BLACK, 6);
+        n7 = new Node(Color.BLACK, 7);
+        n8 = new Node(Color.RED, 8);
+        n9 = new Node(Color.BLACK, 9);
+        n6.left = n2;
+        n6.right = n8;
+        n2.left = n0;
+        n2.right = n4;
+        RedBlackTree b = new RedBlackTree();
+        boolean valid = b.validate(n6);
+        Assert.assertTrue(valid);
     }
 }
