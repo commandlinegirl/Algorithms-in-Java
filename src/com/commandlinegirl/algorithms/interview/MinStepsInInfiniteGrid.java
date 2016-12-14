6c12b6f23e08b49f9a4d625186846eb610c9fa60
Aleksandra Zalcman <biokere@gmail.com>
2016-12-14 11:50:32 -0800

    Min steps in infinite grid

--- src/com/commandlinegirl/algorithms/interview/MinStepsInInfiniteGrid.java
+++ src/com/commandlinegirl/algorithms/interview/MinStepsInInfiniteGrid.java
@@ -1,7 +1,20 @@
 package com.commandlinegirl.algorithms.interview;
 
+import java.util.List;
+
 /**
- * Created by kereish on 12/14/16.
+ * https://www.interviewbit.com/problems/min-steps-in-infinite-grid/
  */
 public class MinStepsInInfiniteGrid {
+
+    // X and Y co-ordinates of the points in order.
+    // Each point is represented by (X.get(i), Y.get(i))
+    public int coverPoints(List<Integer> X, List<Integer> Y) {
+        // iterate over all coordinate points
+        int steps = 0;
+        for (int i = 0; i < X.size() - 1; i++) {
+            steps += Math.max(Math.abs(X.get(i) - X.get(i + 1)), Math.abs(Y.get(i) - Y.get(i + 1)));
+        }
+        return steps;
+    }
 }
